1fe247ffaebb7765352a1f0683f71e64e630de5b
Manish Kumar <krshmanish@gmail.com>
2018-02-17 16:43:19 +0530

    header css fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,13 +11,33 @@ export class HeaderComponent implements OnInit {
   isHome:boolean;
   isOffer: boolean;
   isOrder: boolean;
-  constructor() { 
-    this.isHome = true;
-    this.isOrder = false;
-    this.isOffer = false;
+  constructor(private router: Router) { 
+    if(this.router.url.indexOf('offers')>0) {
+      this.isOffer = true;
+    } else if(this.router.url.indexOf('orders')>0) {
+      this.isOrder = true;
+    } else {
+      this.isHome = true;
+    }
   }
 
   ngOnInit() {
   }
 
+  headerButtonClicked(name) {
+    this.isHome = false;
+    this.isOrder = false;
+    this.isOffer = false;
+    switch(name) {
+      case 'HOME':
+          this.isHome = true;
+      break;
+      case 'ORDER':
+          this.isOrder = true;
+      break;
+      case 'OFFER':
+          this.isOffer = true;
+      break;
+    }
+  }
 }
